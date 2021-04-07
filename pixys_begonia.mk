75a225a1f97ca0b4bb93b3801ea3d6a096583f5f
Itachi Uchiha <harshitverma54@gmail.com>
2021-04-07 08:29:52 +0000

    begonia: Updated FP from sunfish april
    Signed-off-by: Itachi Uchiha <harshitverma54@gmail.com>

--- pixys_begonia.mk
+++ pixys_begonia.mk
@@ -37,10 +37,10 @@ PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/sunfish/sunfish:11/RQ2A.210305.006/7119741:user/release-keys"
+BUILD_FINGERPRINT := "google/sunfish/sunfish:11/RQ2A.210405.005/7181113:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sunfish-user 11 RQ1A.210305.006 7119741 release-keys" \
+    PRIVATE_BUILD_DESC="sunfish-user 11 RQ2A.210405.005 7181113 release-keys" \
     PRODUCT_NAME="begonia" \
     TARGET_DEVICE="begonia"
 
